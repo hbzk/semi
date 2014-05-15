c5f70ffdecb4f477e95a0985530c8fd04c4e25b3
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-05-15 15:52:46 +0900

    .

--- SemiLog/WebContent/www/js/common.js
+++ SemiLog/WebContent/www/js/common.js
@@ -3,8 +3,13 @@ $(function(){
 
 	$("#menuLeft").click(function() {
 		$("#slide").toggle("fade");
-
 	});
+	
+	$("#input").click(function(){
+		$("#input").val("");
+	});
+	
+	
 
 
 
@@ -28,65 +33,13 @@ $(function(){
 	$('#input').autocomplete({
 		source: availableTags,
 		select: function (event, ui) {
-			//아이템 선택시 처리 코드
-
-			if($("#input").val() == "art" || $("#input").val() == "미술") {
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i1").css("display", "");
-			} else if($("#input").val() == "bike" || $("#input").val() == "바이크"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i2").css("display", "");
-			} else if($("#input").val() == "bikerC" || $("#input").val() == "자전거"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i3").css("display", "");
-			} else if($("#input").val() == "booklet" || $("#input").val() == "책"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i4").css("display", "");
-			} else if($("#input").val() == "briefcase" || $("#input").val() == "근무"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i5").css("display", "");
-			} else if($("#input").val() == "brush" || $("#input").val() == "공부"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i6").css("display", "");
-			} else if($("#input").val() == "car" || $("#input").val() == "운전"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i7").css("display", "");
-			} else if($("#input").val() == "computer" || $("#input").val() == "컴퓨터"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i8").css("display", "");
-			} else if($("#input").val() == "game" || $("#input").val() == "게임"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i9").css("display", "");
-			} else if($("#input").val() == "micro" || $("#input").val() == "노래방"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i10").css("display", "");
-			} else if($("#input").val() == "motorcy" || $("#input").val() == "오토바이"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i11").css("display", "");
-			} else if($("#input").val() == "running" || $("#input").val() == "달리기"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i12").css("display", "");
-			} else if($("#input").val() == "scooter" || $("#input").val() == "스쿠터"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i13").css("display", "");
-			} else if($("#input").val() == "train" || $("#input").val() == "전철"){
-				$(".img").css("display", "none");
-				$("hr").css("display", "none");
-				$(".i14").css("display", "");
-			} 
+			
+				if(ui.item.value) {
+					$(".img").css("display", "none");
+					$("hr").css("display", "none");
+					$("." + ui.item.value).css("display", "");
+				}
+			
 		},
 		selectFirst: true,
 		minLength: 1,
