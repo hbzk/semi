48ae2b5b3a7e55677847a8710b0c9355c3105719
hbzk <hb_z@me.com>
2014-05-16 17:49:48 +0900

    드래그 버그 수정, 주석 추가

--- SemiLog/WebContent/www/js/cs_drag.js
+++ SemiLog/WebContent/www/js/cs_drag.js
@@ -17,9 +17,8 @@ $(window).load(function(){
 // 드래그
 function dragdrop_doing() {
 	$('.iconMain').draggable({distance: 20}, {revert: true}, {zIndex: 9},
-		{start: function(event,ui) {
-		
-	}});
+		// 드래그 시작시 동작 함수
+		{start: function(event,ui) {}});
 }
 
 // 드롭
@@ -37,18 +36,16 @@ function dragdrop_drop() {
 			.removeClass('ui-draggable ui-draggable-dragging');
 
 		
-		
-		$('.iconStart').droppable({ disabled: true });
-		$('.iconStart').draggable({distance: 20}, {revert: "invalid"}, {zIndex: 9},
-			{
-			start: function(event,ui) {}, stop: function(event, ui) {}
-		});
+		// 중앙아이콘 - 드롭 비활성. 드래그 활성 
+		$('.iconStart').droppable({ disabled: true }).draggable({distance: 20},{revert: "invalid"}, {zIndex: 9},
+			{start: function(event,ui) {}, stop: function(event, ui) {}});
 	}});
 }
 
-//중앙 클릭시
+// 중앙 클릭시
 function dragdrop_startClick() {
 	$('.iconStart').click(function(){
+		// 이미지가 있는지 확인 후, 없을때만 채우기 (깜빡임 방지)
 		if ($('.iconStart').css('background-image') != startImg) {
 			$('.iconStart').css('background-image', startImg);
 		}
@@ -57,17 +54,18 @@ function dragdrop_startClick() {
 			$(lastdrager).css('background-image', lastdragerImg);
 		}
 		
-		dragdrop_doing();
-		$('.iconStart').droppable({ disabled: false });
-		
-		//타이머 초기화 후 사라짐
-		
+		// 타이머 초기화 후 사라짐
 		if (typeof $id === 'undefined') {
 			console.log('undefined');
 		} else {
 			reset();
 			$('#result').html('').removeClass();
 		}
+		
+		// 중앙아이콘 - 드래그 비활성, 드롭 활성 
+		dragdrop_doing();
+		$('.iconStart').draggable({ disabled: true }).droppable({ disabled: false });
+		
 		
 	});
 }
