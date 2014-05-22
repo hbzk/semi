161bca62f6d7029dc171b3c9889e0274408517cd
hbzk <hb_z@me.com>
2014-05-22 16:33:22 +0900

    font에 드래그 적용

--- SemiLogServer/WebContent/www/js/cs_drag_font.js
+++ SemiLogServer/WebContent/www/js/cs_drag_font.js
@@ -1,17 +1,18 @@
 var startIcon;
 var lastIcon;
 var lastDragger;
+var lastDraggerClass;
 
 $(window).load(function(){
-	startIcon = ($('#start').children('i'));
+	startIcon = $('#start').html();
 	
 	dragdrop_doing();
 	dragdrop_drop();
-
-	$('#start').click(function(){ // 시작 버튼 클릭시 초기화
+	
+	$('#middle').mouseup(function(){ // 미들 클릭시 초기화
 		dragdrop_timerCheck();
 	});
-
+	
 });
 
 
@@ -22,51 +23,28 @@ function dragdrop_doing() {
 
 // 드롭
 function dragdrop_drop() {
-	$('#start').droppable({tolerance: "touch"}, {drop: function(event, ui){
+	$('#start').droppable({tolerance: 'touch'}, {accept: '.iconMain'}, {drop: function(event, ui){
 		
-		/*dragdrop_timerCheck(); */// 이미 실행중인지 확인 후 초기화
+		dragdrop_timerCheck(); // 이미 실행중인지 확인 후 초기화
 		
 		// 드래그 대상 관련
 		lastIcon = $(event.toElement);
 		lastDragger = lastIcon.parent();
+		lastDragger.draggable({revertDuration:0});
+		lastDraggerClass = lastDragger[0].className;
 		
-		$('#middle').html(lastIcon)
-		.draggable({distance: 20}, {revert: true}, {revertDuration: 500}, {zIndex: 9}); // 해당 아이콘 중앙 배치
-		
-		
-/*		lastdragerIcon = lastdrager.css('background-image');
-		lastdrager.css('background-image', 'none');
-		lastdragerIconClass = lastdrager.context.className;
-		lastdrager.draggable({revertDuration:0}); */
-		
-		
-		
+		// 아이콘 중앙 배치
+		$('#middle').html(lastIcon).draggable({zIndex: 9});
 		
 		// 시작 아이콘 관련
-		$('#start').html();
-		
-		/*$('#start').css('background-image', lastdragerIcon)
-			.draggable({disabled: false},{distance: 20},{revert: "invalid"},{zIndex: 9});*/
+		$('#start').children().remove();
 		
 		// 타이머 출력
-		/*timer_doing('timer');
-		$('#timer').addClass(lastdragerIconClass)
-			.removeClass('ui-draggable ui-draggable-dragging');*/
+		timer_doing('timer');
+		$('#timer').addClass(lastDraggerClass)
+			.removeClass('ui-draggable ui-draggable-dragging');
 		
 	}});
-}
-
-// 빙글빙글
-function dragdrop_flip() {
-	lastdrager.addClass('flipping');
-	$('.iconMain').draggable({disabled:true}); // 전체 드래그 비활성화
-	
-	// 일정 시간 후 다시 드래그 활성화
-	setTimeout(function(){
-		$('.flipping').removeClass('flipping');
-		$('.iconMain').draggable({disabled: false});
-		$('#timer').draggable({disabled: true});
-	}, 1000);
 }
 
 // 타이머 구동 확인 + 저장 + 초기화
@@ -76,13 +54,23 @@ function dragdrop_timerCheck() {
 		timer_reset(); // 타이머 초기화
 		$('#timer').html('').removeClass(); // 타이머 출력 제거
 		
-		dragdrop_doing(); // 드래그 항목 갱신 (타이머 드래그 방지)
-		
-		$('#start').draggable({disabled: true}).droppable({disabled: false}) // 시작아이콘 드래그 방지
-			.css('background-image', startImg); // 시작 아이콘 초기화
-		lastdrager.css('background-image', lastdragerIconClass); // 드래그했던 아이콘 초기화
-		
-		
+		// 시작 아이콘 복구
+		$('#start').html(startIcon); 
+		$('#middle').attr('style','').removeClass();
+		lastDragger.html(lastIcon);
+
 		dragdrop_flip(); // 빙글빙글
 	}
+}
+
+//빙글빙글
+function dragdrop_flip() {
+	lastDragger.addClass('flipping');
+	$('.iconMain').draggable({disabled:true}); // 전체 드래그 비활성화
+	
+	// 일정 시간 후 다시 드래그 활성화
+	setTimeout(function(){
+		$('.flipping').removeClass('flipping');
+		$('.iconMain').draggable({disabled: false});
+	}, 1000);
 }
