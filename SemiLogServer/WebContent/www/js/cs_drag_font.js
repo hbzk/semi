169dd3828ac2262e3ef1720d3401c1b43d75b7ed
hbzk <hb_z@me.com>
2014-05-22 18:02:26 +0900

    드래그 버그 수정

--- SemiLogServer/WebContent/www/js/cs_drag_font.js
+++ SemiLogServer/WebContent/www/js/cs_drag_font.js
@@ -18,18 +18,27 @@ $(window).load(function(){
 
 // 드래그 대상 설정
 function dragdrop_doing() {
-	$('.iconMain').draggable({distance: 20}, {revert: true}, {revertDuration: 500}, {zIndex: 9});
+	$('.drag').draggable({distance: 20}, {revert: true}, {revertDuration: 500}, {zIndex: 9});
 }
 
 // 드롭
 function dragdrop_drop() {
-	$('#start').droppable({tolerance: 'touch'}, {accept: '.iconMain'}, {drop: function(event, ui){
+	$('#start').droppable({tolerance: 'touch'}, {accept: '.drag'}, {drop: function(event, ui){
 		
 		dragdrop_timerCheck(); // 이미 실행중인지 확인 후 초기화
 		
 		// 드래그 대상 관련
 		lastIcon = $(event.toElement);
-		lastDragger = lastIcon.parent();
+		
+		console.log(lastIcon.parent('div'));
+		
+		// 의도치 않게 드래거 인식 실패하는 경우 대비
+		if (lastIcon.parent('div').length > 0) {
+			lastDragger = lastIcon.parent('div');
+		} else {
+			lastDragger = lastIcon.parent('div').context;
+		}
+		
 		lastDragger.draggable({revertDuration:0});
 		lastDraggerClass = lastDragger[0].className;
 		
@@ -41,9 +50,9 @@ function dragdrop_drop() {
 		
 		// 타이머 출력
 		timer_doing('timer');
+		console.log(lastDraggerClass);
 		$('#timer').addClass(lastDraggerClass)
-			.removeClass('ui-draggable ui-draggable-dragging');
-		
+			.removeClass('drag ui-draggable ui-draggable-dragging');
 	}});
 }
 
@@ -57,7 +66,9 @@ function dragdrop_timerCheck() {
 		// 시작 아이콘 복구
 		$('#start').html(startIcon); 
 		$('#middle').attr('style','').removeClass();
+		
 		lastDragger.html(lastIcon);
+		lastDragger.attr('style','');
 
 		dragdrop_flip(); // 빙글빙글
 	}
@@ -66,11 +77,11 @@ function dragdrop_timerCheck() {
 //빙글빙글
 function dragdrop_flip() {
 	lastDragger.addClass('flipping');
-	$('.iconMain').draggable({disabled:true}); // 전체 드래그 비활성화
+	$('.drag').draggable({disabled:true}); // 전체 드래그 비활성화
 	
 	// 일정 시간 후 다시 드래그 활성화
 	setTimeout(function(){
 		$('.flipping').removeClass('flipping');
-		$('.iconMain').draggable({disabled: false});
+		$('.drag').draggable({disabled: false});
 	}, 1000);
 }
