3f84e30a58f046c7d71816d46289e0f78b175d3c
sem1ovely <k1msem1x@gmail.com>
2014-05-26 16:41:52 +0900

    Login -> using nodeJS passport

--- SemiLogServer/WebContent/www/js/login.js
+++ SemiLogServer/WebContent/www/js/login.js
@@ -1,53 +1,100 @@
-var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var path = require('path');
+
+//get passport for LOGIN
+var passport = require('passport')
+, LocalStrategy = require('passport-local').Strategy;
+
+var mysql = require('mysql');
+var sha1 = require('./sha1.js');
 
 var app = express();
-app.use(express.bodyParser());
 
-var server = http.createServer(function(request, response) {
-    if (request.method == 'POST') {
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-        });
-        request.on('end', function () {
-            var req = qs.parse(body);
-            checkAuth (req, res, next);
-        });
-    } else {
-        var url_parts = url.parse(request.url, true);
-        var query = url_parts.query;
-        console.log(query);
-        checkAuth (query, res, next);
-    }
+
+app.use(bodyParser());
+
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+var dbconn = mysql.createConnection({
+	host : 'localhost',
+	user : 'semi',
+	password : 'semi',
+	database:'semidb'
 });
 
 
-function checkAuth (req, res, next){
-	if(!req.sesstion.user_id){
-		res.send('Not authorized to SemiColon');
-	}else{
-		res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-		next();
-	}
-}
-app.use(app.router);
+// /signup post 요청 오면 insert 수행
+app.post('/signup',function(req,res){
+	
+	var user = req.body;
+    dbconn.query('insert into USER set ?', user, function(err,result){
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        res.send(200,'success');
+    });
+});
 
-app.get('/afterLogin', checkAuth, function (req, res) {
-	  res.send('if you are viewing this page it means you are logged in');
+
+// (테스트 할 동안) pathname 없으면 user list 출력
+app.get('/', function(req,res){
+	
+	dbconn.query('select * from USER', function(err, rows){
+		if (err) {
+			console.log('Query err');
+			console.log(err);
+		}
+		console.log(rows);
+		res.json(rows);
 	});
+});
 
-app.post('/login', function (req, res) {
-	  var post = req.body;
-	  if (post.user === 'id' && post.password === 'password') {
-	    req.session.user_id = johns_user_id_here;
-	    res.redirect('/afterLogin');
-	  } else {
-	    res.send('Bad user/pass');
-	  }
-	});
+module.exports = function(passport) {
+ 
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+		done(null, user.id);
+    });
+ 
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+		connection.query("select * from users where id = "+id,function(err,rows){	
+			done(err, rows[0]);
+		});
+    });
+    
+    passport.use('/login', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+ 
+         connection.query("SELECT * FROM `users` WHERE `email` = '" + email + "'",function(err,rows){
+			if (err)
+                return done(err);
+			 if (!rows.length) {
+                return done(null, false, req.flash('loginMessage', 'No user found.')); 
+                // req.flash is the way to set flashdata using connect-flash
+            } 
+			
+			// if the user is found but the password is wrong
+            if (!( rows[0].password == password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); 
+            // create the loginMessage and save it to session as flashdata
+			
+            // all is well, return successful user
+            return done(null, rows[0]);			
+		
+		});
+ 
+ 
+    }));
+ 
+};
 
-app.get('/logout', function (req, res) {
-	  delete req.session.user_id;
-	  res.redirect('/login');
-	});    
+app.listen(3000);
+console.log('Server ON : 3000');
