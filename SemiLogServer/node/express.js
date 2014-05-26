905c3f6d90c7c79f0e2a471593e24432ef11d8ca
hbzk <hb_z@me.com>
2014-05-26 19:24:01 +0900

    signup - email 중복 검증, 입력칸 빈칸일때 submit disabled

--- SemiLogServer/node/express.js
+++ SemiLogServer/node/express.js
@@ -21,6 +21,11 @@ var dbconn = mysql.createConnection({
 
 // /signup으로 post 요청 오면 insert 수행
 app.post('/signup',function(req,res){
+	
+	console.log(req.body);
+	console.log(req.body.email);
+	console.log(req.body.password);
+	
 	req.body.password = sha1.SHA1(req.body.password); // 암호화 
 	
 	var user = req.body;
@@ -39,11 +44,13 @@ app.post('/login', function(req,res){
 	console.log('login post');
 	
 	var email = req.body.email;
+	var password = sha1.SHA1(req.body.password);
+	console.log(password);
 	dbconn.query('select * from USER where EMAIL = ?', email, function(err, rows, fields){
 		if (err) {console.log(err);}
 		if (!rows.length) { res.send('가입 안된 이메일'); } 
 		else {
-			if (rows[0].PASSWORD != req.body.password) {
+			if (rows[0].PASSWORD != password) {
 				res.send('암호가 다르다');
 			} else {
 				res.cookie('email', req.body.email);
@@ -63,6 +70,25 @@ app.get('/emailCheck',function(req,res){
 		if (!rows.length) { res.send('가입 안된 이메일'); } 
 		else { res.send('이미 있는 이메일'); }
 	});
+});
+
+app.post('/emailCheck',function(req,res){
+	console.log('/emailCheck -> post');
+	console.log(req.body.email);
+	
+	if (req.body.email != '') {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		var email = req.body.email;
+		dbconn.query('select * from USER where EMAIL = ?', email, function(err, rows){
+			if (err) {console.log(err);}
+			if (rows.length) { 
+				res.send('err'); 
+			} 
+			else { 
+				res.send('가입 안된 이메일'); 
+			}
+		});
+	}
 });
 
 
