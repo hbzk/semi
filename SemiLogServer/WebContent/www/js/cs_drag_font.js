3caf16398d4be693a691ec35acf23d0e5ecd6aad
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-05-29 18:11:18 +0900

    .

--- SemiLogServer/WebContent/www/js/cs_drag_font.js
+++ SemiLogServer/WebContent/www/js/cs_drag_font.js
@@ -1,115 +1,3 @@
-var now = new Date();
-var minute = now.getMinutes().toString();
-var second = now.getSeconds().toString();
-minute = 1;
-second = 00;
-end=0;
-
-function timeclock(){
-  if(second == 00) {
-    minute -= 1 ;
-    second = 2 ;
-  } else{
-    second = second-1;
-  }
-  
-  
-  if ((minute < 0) && (end==0)) {
-	  BnV();
-	  showConfirm();
-    end=1;
-  }
-  
-  
-  if (second < 10) {
-    document.clock.txtSecs.value = 0 + second.toString();
-  } else {
-    document.clock.txtSecs.value = second;
-  }
-  if (minute < 10) {
-      document.clock.txtMins.value = 0 + minute.toString();
-    } else {
-      document.clock.txtMins.value = minute;
-    }
-  
-  
-  
-  setTimeout("timeclock()", 1000);
-}
-
-
-
-
-
-
-
-
-document.addEventListener("deviceready", onDeviceReady, false);
-
-
-function onDeviceReady() {
-	 navigator.notification.beep(3);
-	 navigator.notification.vibrate(2000);
-}
-
-
-
-// Beep three times
-//
-function playBeep() {
-    navigator.notification.beep(3);
-}
-
-// Vibrate for 2 seconds
-//
-function vibrate() {
-    navigator.notification.vibrate(2000);
-}
-
-function BnV() {
-	 navigator.notification.beep(3);
-    navigator.notification.vibrate(2000);
-}
-
-//process the confirmation dialog result
-function onConfirm(buttonIndex) {
-    alert('You selected button ' + buttonIndex);
-}
-
-// Show a custom confirmation dialog
-//
-function showConfirm() {
-	 navigator.notification.vibrate(2000);
-    navigator.notification.confirm(
-        'You are the winner!', // message
-         onConfirm,            // callback to invoke with index of button pressed
-        'Game Over',           // title
-        ['Restart','Exit']         // buttonLabels
-    );
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* --------------------------timer------------------------------ */
-
-
-
-
-
-
-
 var startIcon;
 var lastIcon;
 var lastDragger;
@@ -215,6 +103,104 @@ function dragdrop_flip() {
 
 
 
+/* --------------------------timer------------------------------ */
+
+
+
+
+
+
+
+
+
+var now = new Date();
+var minute = now.getMinutes().toString();
+var second = now.getSeconds().toString();
+minute = 1;
+second = 00;
+end=0;
+
+function timeclock(){
+  if(second == 00) {
+    minute -= 1 ;
+    second = 2 ;
+  } else{
+    second = second-1;
+  }
+  
+  
+  if ((minute < 0) && (end==0)) {
+	  showConfirm();
+    end=1;
+  }
+  
+  
+  if (second < 10) {
+    document.clock.txtSecs.value = 0 + second.toString();
+  } else {
+    document.clock.txtSecs.value = second;
+  }
+  if (minute < 10) {
+      document.clock.txtMins.value = 0 + minute.toString();
+    } else {
+      document.clock.txtMins.value = minute;
+    }
+  
+  
+  
+  setTimeout("timeclock()", 1000);
+}
+
+
+
+
+
+
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+function onDeviceReady() {
+
+}
+
+
+
+// Beep three times
+//
+function playBeep() {
+    navigator.notification.beep(3);
+}
+
+// Vibrate for 2 seconds
+//
+function vibrate() {
+    navigator.notification.vibrate(2000);
+}
+
+function BnV() {
+	 navigator.notification.beep(3);
+    navigator.notification.vibrate(2000);
+}
+
+//process the confirmation dialog result
+function onConfirm(buttonIndex) {
+    alert('You selected button ' + buttonIndex);
+}
+
+// Show a custom confirmation dialog
+//
+function showConfirm() {
+	 navigator.notification.vibrate(2000);
+    navigator.notification.confirm(
+        'You are the winner!', // message
+         onConfirm,            // callback to invoke with index of button pressed
+        'Game Over',           // title
+        ['Restart','Exit']         // buttonLabels
+    );
+    
+}
 
 
 
