99153108d782cac8f3cea4e28f1568afb435a2a8
hbzk <hb_z@me.com>
2014-06-13 17:14:09 +0900

    변경 된 DB에서 메인 페이지 아이콘 로드 - 구현

--- SemiLogServer/WebContent/www/js/loadSelectedIcon.js
+++ SemiLogServer/WebContent/www/js/loadSelectedIcon.js
@@ -2,46 +2,21 @@
 var db = window.openDatabase("Database", "1.0", "LogDB", 2 * 1024 * 1024);
 
 $(window).load(function(){
-	db_icon_init();
-	loadSelectedIcon();
+	loadMainIcon();
 });
 
-function loadSelectedIcon(){
+function loadMainIcon(){
 	var iconDiv = $("#iconMainDiv .drag");
 	console.log("Load DB - data (selectedIcon)");
 	db.transaction(function(tx){
-		tx.executeSql('SELECT * FROM ICONSELECT',
-				[],
-				function(tx, rs){
-					for( var i=0 ; i < rs.rows.length ; i++){
-						var row = rs.rows.item(i);
-						console.log("className = " + row.ICON_NAME +"///" + " className = " + row.CLASS_NAME);
-						
-						$($(iconDiv)[i]).append("<i data-name = '"+ row.ICON_NAME +"' class = '"+ row.CLASS_NAME +"'></i>");
-					}
-				});
-	});
-}
-
-function db_icon_init() {
-	db.transaction(function(tx) {
-		//tx.executeSql('drop table if exists ICONSELECT'); // DB 초기화
-		tx.executeSql('create table if not exists ICONSELECT (NO integer unique, ICON_NAME text, CLASS_NAME text)');
-		
-		tx.executeSql('INSERT OR IGNORE INTO ICONSELECT '  
-			+ ' SELECT 1 AS NO, "flask" AS ICON_NAME, "fa fa-flask" AS CLASS_NAME '
-			+ ' UNION SELECT 2,"code","fa fa-code" '
-			+ ' UNION SELECT 3,"desktop","fa fa-desktop" '
-			+ ' UNION SELECT 4,"home","fa fa-home" '
-			+ ' UNION SELECT 5,"stethoscope","fa fa-stethoscope" '
-			+ ' UNION SELECT 6,"keyboard","fa fa-keyboard-o" '
-		);
-
-		/*tx.executeSql('INSERT OR IGNORE INTO ICONSELECT (NO, ICON_NAME, CLASS_NAME) VALUES (1,"flask","fa fa-flask")');
-		tx.executeSql('INSERT OR IGNORE INTO ICONSELECT (NO, ICON_NAME, CLASS_NAME) VALUES (2,"code","fa fa-code")');
-		tx.executeSql('INSERT OR IGNORE INTO ICONSELECT (NO, ICON_NAME, CLASS_NAME) VALUES (3,"desktop","fa fa-desktop")');
-		tx.executeSql('INSERT OR IGNORE INTO ICONSELECT (NO, ICON_NAME, CLASS_NAME) VALUES (4,"home","fa fa-home")');
-		tx.executeSql('INSERT OR IGNORE INTO ICONSELECT (NO, ICON_NAME, CLASS_NAME) VALUES (5,"stethoscope","fa fa-stethoscope")');
-		tx.executeSql('INSERT OR IGNORE INTO ICONSELECT (NO, ICON_NAME, CLASS_NAME) VALUES (6,"keyboard","fa fa-keyboard-o")');*/
+		tx.executeSql('SELECT * FROM ICONLIST', [], function(tx, rs){
+			for( var i=0 ; i < rs.rows.length ; i++){
+				var row = rs.rows.item(i);
+				
+				if (row.POSITION > 0) {
+					$($(iconDiv)[row.POSITION - 1]).append("<i data-name = '"+ row.ICON_NAME +"' class = '"+ row.CLASS_NAME +"'></i>");
+				}
+			}
+		});
 	});
 }
