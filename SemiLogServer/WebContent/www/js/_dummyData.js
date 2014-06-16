79f622dac061ef7c5ce3ebe46a45b62353f22bc2
hbzk <hb_z@me.com>
2014-06-16 17:47:34 +0900

    주석 추가

--- SemiLogServer/WebContent/www/js/_dummyData.js
+++ SemiLogServer/WebContent/www/js/_dummyData.js
@@ -6,11 +6,12 @@ $(document).ready(function(){
 
 function dummy() {
 	db.transaction(function(tx) {
+		tx.executeSql('DROP TABLE IF EXISTS ACTION ');
 		tx.executeSql('create table if not exists ACTION (id integer primary key, TITLE text, CLASSNAME text, START_TIME date, END_TIME date, WHILE integer)');
 		
 		// 여기부터 
 		tx.executeSql("INSERT INTO ACTION (TITLE, CLASSNAME, START_TIME, END_TIME, WHILE) "
-			+ " VALUES ('home', 'fa fa-home', '2014-05-05T08:23:06.559Z', '2014-05-05T08:23:06.559Z', 30)");
+			+ " VALUES ('home', 'fa fa-home', '2014-05-05T08:23:06.559Z', '2014-05-05T08:23:06.559Z', 1)");
 		// 여기까지
 		
 		
