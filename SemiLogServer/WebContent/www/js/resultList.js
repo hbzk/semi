820dae949504c8656fc8c3baf1ab3a5572b20d68
hbzk <hb_z@me.com>
2014-06-17 11:52:03 +0900

    List 페이징 구현

--- SemiLogServer/WebContent/www/js/resultList.js
+++ SemiLogServer/WebContent/www/js/resultList.js
@@ -1,82 +1,115 @@
-var actionName, startTime, endTime, resultWhile;
 var db = window.openDatabase("Database", "1.0", "LogDB", 2 * 1024 * 1024);
+var actionName, startTime, endTime, resultWhile, resultDate;
 var dbId, clickedTable; 
+var pageList = new Array();
 
+db_pageList();	// 페이징을 위한 전체 목록
+console.log(pageList);
 db_selectLastDay();		// 마지막 날 출력
-/*searchS = '2014-06-12';
-db_listText();*/
+
+$('#date .left').click(function(){
+	var target = pageList[$.inArray(resultDate, pageList) - 1];
+	console.log(target);
+	db_selectSearch(target, '+1 day');
+});
+
+$('#date .right').click(function(){
+	var target = pageList[$.inArray(resultDate, pageList) + 1];
+	console.log(target);
+	db_selectSearch(target, '+1 day');
+});
 
 $('#search').click(function(){
 	var searchText = $('#searchText').val();
-	db_selectSearch(searchText);
+	db_selectSearch(searchText, '+1 day');
 });
 
 $('#deleteAll').click(function(){
-	db_deleteAll();
-	location.reload(true);
+	if (confirm('정말 다 지움?')) {
+		db.transaction(function(tx) {
+			tx.executeSql("DELETE FROM ACTION");
+		}, db_errorCB);
+		location.reload(true);
+	}
 });
 
-
 $(document).on("click",".rtDelete",function(e){
-	dbId = $(e.target).parent(".rtDelete").siblings(".rtIcon")[0].attributes[0].value;
-	console.log(dbId);
-	clickedTable = $(e.target).parent(".rtDelete").parent(".rtTable");
-	console.log(clickedTable);
-	db_delete(dbId);
-}); 
+	if (confirm('정말 지움?')) {
+		dbId = $(e.target).parent(".rtDelete").siblings(".rtIcon")[0].attributes[0].value;
+		clickedTable = $(e.target).parent(".rtDelete").parent(".rtTable");
+		db_delete(dbId);
+	}
+});
  
  $(document).on("click",".rtTable",function(){
 	 $(clickedTable).fadeOut(700);
  });
 
-function db_deleteAll(){
-	db.transaction(function(tx) {
-		tx.executeSql("DELETE FROM ACTION");
-	}, db_errorCB);
+
+// ========================= 함수 정의 
+function db_delete(no){
+	db.transaction(function(tx){
+		tx.executeSql("DELETE FROM ACTION WHERE id = ?",	[no]);
+	});
 }
 
-
-function db_selectSearch(date) {
+function db_pageList() { // 페이징 용 날짜 목록 만들기
 	db.transaction(function(tx) {
-		tx.executeSql("SELECT * FROM ACTION WHERE START_TIME BETWEEN date(?) AND date(?, ?)", [date, date, '+1 day'], function(tx, res) {
-			db_listing(res);
+		tx.executeSql("SELECT strftime('%Y-%m-%d', START_TIME) AS strtDay FROM ACTION ORDER BY strtDay", [], function(tx, res) {
+			var len = res.rows.length;
+			console.log("ACTION (All): " + len + " rows found.");
+			
+			for (var i=0; i<len; i++){
+				if ($.inArray(res.rows.item(i).strtDay, pageList) == -1) {
+					pageList.push(res.rows.item(i).strtDay);
+				}
+			}
+			//console.log(pageList);
 		}, db_errorCB);
 	});
 };
 
-function db_selectLastDay() {
+function db_selectLastDay() { // 마지막 행동이 있는 날짜 출력 
 	var lastActionSql = "(SELECT date(START_TIME) AS stDay FROM ACTION ORDER BY START_TIME DESC LIMIT 1)";
 	db.transaction(function(tx) {
-		tx.executeSql("SELECT * FROM ACTION WHERE START_TIME BETWEEN date("+lastActionSql+") AND date("+lastActionSql+", ?)", ['+1 day'], function(tx, res) {
-			db_listing(res);
+		tx.executeSql("SELECT *, strftime('%Y-%m-%d', START_TIME) AS strtDay FROM ACTION "
+			+" WHERE START_TIME BETWEEN date("+lastActionSql+") AND date("+lastActionSql+", ?) ORDER BY strtDay", ['+1 day'], function(tx, res) {
+				db_listing(res);
 		}, db_errorCB);
 	});
 };
 
-/*function db_selectLastDay() {
+function db_selectSearch(date, range) { // 날짜, 범위 받고 출력
 	db.transaction(function(tx) {
-		tx.executeSql("SELECT date(START_TIME) AS stDay FROM ACTION ORDER BY START_TIME DESC LIMIT 1", [], function(tx, res) {
-			var searchDay = res.rows.item(0).stDay;
-			
-			tx.executeSql("SELECT * FROM ACTION WHERE START_TIME BETWEEN date(?) AND date(?, ?)", [searchDay, searchDay, '+1 day'], function(tx, res) {
+		tx.executeSql("SELECT *, strftime('%Y-%m-%d', START_TIME) AS strtDay FROM ACTION "
+			+" WHERE START_TIME BETWEEN date(?) AND date(?, ?) ORDER BY strtDay", [date, date, range], function(tx, res) {
 				db_listing(res);
-			}, db_errorCB);
-			
 		}, db_errorCB);
 	});
-};*/
-
+};
 
 // 결과를 HTML에 출력
-function db_listing(res, lastDay) {
+function db_listing(res) {
 	var len = res.rows.length;
-	console.log("ACTION: " + len + " rows found.");
+	console.log("ACTION (page): " + len + " rows found.");
+	
+	resultDate = res.rows.item(0).strtDay;
+	if ($.inArray(resultDate, pageList) == 0) {
+		$('#date .left').css('display', 'none');
+	} else {
+		$('#date .left').css('display', '');
+	} 
+		
+	if ($.inArray(resultDate, pageList) > pageList.length -2) {
+		$('#date .right').css('display', 'none');
+	} else {
+		$('#date .right').css('display', '');
+	}
+	
+	$('#date>p').text(resultDate.replace(/-/g, '/').substring(5));
 	
 	var resultList = $('#resultList');
 	resultList.html('');	// 리스트 초기화
-	
-	var resultDate = res.rows.item(len-1).START_TIME.substring(5,10); // 월-일 형태로 추출
-	$('#date>p').text(resultDate);
 	
 	for (var i=0; i<len; i++){
 		startTime = new Date(res.rows.item(i).START_TIME);
@@ -108,12 +141,3 @@ function db_nextDay(day) {
 	return day.substring(0, 9).concat(parseInt(day.charAt(9)) + 1);
 }
 
-function db_delete(no){
-	db.transaction(function(tx){
-		tx.executeSql("DELETE FROM ACTION WHERE id = ?",
-									[no],
-									function(){
-										//db_listText();
-									});
-	});
-}
