a81f4c19b751e3ead95ab95ce8457f4f9fe24ac7
hbzk <hb_z@me.com>
2014-06-24 15:16:54 +0900

    express signup 로직 변경

--- DragLogSVN/node/express.js
+++ DragLogSVN/node/express.js
@@ -24,35 +24,28 @@ app.get('/createUser',function(req,res){
 	dbconn.query('INSERT INTO USER SET PASSWORD=""', function(err, rows){
 		if (err) {
 			console.log(err);
-            next(err);
             throw err;
 		}
 		console.log(rows);
-		console.log(rows.insertId);
 		res.send(rows.insertId.toString());
 	});
 });
 
-
-// /signup으로 post 요청 오면 insert 수행
+// /signup으로 post 요청 오면 update 수행
 app.post('/signup',function(req,res){
 	var user = req.body;
 	user.password = sha1.SHA1(user.password); // 암호화 
 	
 	console.log(req.headers);
-	console.log(req.headers.origin);
-	
-	
 	console.log(user);
-    dbconn.query('insert into USER set ?', user, function(err,result){
+    dbconn.query('UPDATE USER SET EMAIL=?, PASSWORD=?, GENDER=?, AGE=?, JOB=? WHERE USER_NO=?', 
+    		[user.email, user.password, user.gender, user.age, user.job, user.user_no], function(err,result){
         if (err) {
             console.log(err);
-            next(err);
             throw err;
         }
         res.redirect(''+req.headers.origin+'/www/main.html');
     });
-
 });
 
 // 로그인
@@ -60,11 +53,9 @@ app.post('/login', function(req,res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	
-	console.log('login post');
-	
 	var email = req.body.email;
 	var password = sha1.SHA1(req.body.password);
-	dbconn.query('select * from USER where EMAIL = ?', email, function(err, rows, fields){
+	dbconn.query('SELECT * FROM USER WHERE EMAIL = ?', email, function(err, rows, fields){
 		if (err) {console.log(err);}
 		if (!rows.length) { res.send('가입 안된 이메일'); } 
 		else {
@@ -77,23 +68,8 @@ app.post('/login', function(req,res){
 				user.PASSWORD = '';
 				
 				res.send(user);
-				//dbconn.query('UPDATE USER SET  WHERE ID = ?', , function(err, rows, fields){
-				
-				// 서버에서 유저 정보 받아서 장비 유저 테이블에 넣기
-				//res.redirect(''+req.headers.origin+'/www/main.html');
 			}
 		}
-	});
-});
-
-
-// (테스트용 / 삭제 예정) 이메일 중복 확인
-app.get('/emailCheck',function(req,res){
-	var email = req.query.email;
-	dbconn.query('select * from USER where EMAIL = ?', email, function(err, rows){
-		if (err) {console.log(err);}
-		if (!rows.length) { res.send('가입 안된 이메일'); } 
-		else { res.send('이미 있는 이메일'); }
 	});
 });
 
@@ -102,7 +78,7 @@ app.post('/emailCheck',function(req,res){
 	if (req.body.email != '') {
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		var email = req.body.email;
-		dbconn.query('select * from USER where EMAIL = ?', email, function(err, rows){
+		dbconn.query('SELECT * FROM USER WHERE EMAIL = ?', email, function(err, rows){
 			if (err) {console.log(err);}
 			if (rows.length) { 
 				res.send('already'); 
@@ -118,7 +94,7 @@ app.post('/emailCheck',function(req,res){
 // (테스트용 / 삭제 예정) pathname 없으면 user list 출력
 app.get('/', function(req,res){
 	
-	dbconn.query('select * from USER', function(err, rows){
+	dbconn.query('SELECT * FROM USER', function(err, rows){
 		if (err) {
 			console.log('Query err');
 			console.log(err);
