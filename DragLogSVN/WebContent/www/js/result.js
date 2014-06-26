5d37298718faf303d1eda5f76a5492f95f5d7609
hbzk <hb_z@me.com>
2014-06-26 11:15:43 +0900

    출력 script 정리, 함수화

--- DragLogSVN/WebContent/www/js/result.js
+++ DragLogSVN/WebContent/www/js/result.js
@@ -1,5 +1,5 @@
 var db = window.openDatabase("Database", "1.0", "LogDB", 2 * 1024 * 1024);
-var actionName, startTime, endTime, resultWhile, resultDate, targetDate, scope, lastActionSql, whereSql;
+var firstResultDate, firstResultDate, targetDate, scope;
 var dbId, clickedTable; 
 var dayList = new Array();
 var weekList = new Array();
@@ -21,55 +21,28 @@ $(document).ready(function(){
 	});
 	
 	$('#date .left').click(function(){
-		if (scope == 'LASTDAY') {
-			scope = 'DAY';
-		}
-		if (scope == 'DAY') {
-			targetDate = dayList[$.inArray(resultDate, dayList) - 1];
-			navCheck(dayList);
-		} else if (scope == 'WEEK') {
-			targetDate = weekList[$.inArray(targetDate, weekList) - 1];
-			navCheck(weekList);
-		} else if (scope == 'MONTH') {
-			targetDate = monthList[$.inArray(targetDate, monthList) - 1];
-			navCheck(monthList);
-		}
-		db_selectSearch(scope, targetDate);
+		nav(-1);
 	});
 
 	$('#date .right').click(function(){
-		if (scope == 'DAY') {
-			targetDate = dayList[$.inArray(resultDate, dayList) + 1];
-			navCheck(dayList);
-		} else if (scope == 'WEEK') {
-			targetDate = weekList[$.inArray(targetDate, weekList) + 1];
-			navCheck(weekList);
-		} else if (scope == 'MONTH') {
-			targetDate = monthList[$.inArray(targetDate, monthList) + 1];
-			navCheck(monthList);
-		}
-		db_selectSearch(scope, targetDate);
+		nav(+1);
 	});
 	
 	// 일, 주, 월 지정
 	$('#day').click(function(){
 		scope = 'DAY';
-		targetDate = dayList[dayList.length-1];
-		navCheck(dayList);
-		db_selectSearch(scope, targetDate);
+		scopeClick(dayList);
+		
 	});
+	
 	$('#week').click(function(){
 		scope = 'WEEK';
-		targetDate = weekList[weekList.length-1];
-		navCheck(weekList);
-		db_selectSearch(scope, targetDate);
+		scopeClick(weekList);
 	});
+	
 	$('#month').click(function(){
-		console.log(monthList);
 		scope = 'MONTH';
-		targetDate = monthList[monthList.length-1];
-		navCheck(monthList);
-		db_selectSearch(scope, targetDate);
+		scopeClick(monthList);
 	});
 	
 	
@@ -92,32 +65,61 @@ $(document).on("click",".rtTable",function(){
 
 // ========================= 함수 정의
 
-function navCheck(list) {
+// scope 클릭시 
+var scopeClick = function(list) {
+	targetDate = list[list.length-1]; 			// 해당 scope 마지막 기록을 타겟으로 설정
+	navDisplay(list); 								// 좌우 화살표 출력 판단
+	db_selectSearch(scope, targetDate); 	// 결과 출력 호출
+};
+
+// 좌/우 nav 클릭시 
+var nav = function (direction) {
+	var list = [];
+	if (scope == 'LASTDAY') scope = 'DAY';
+	
+	// scope에 따른 변수 설정
+	if (scope == 'DAY') list = dayList;
+	else if (scope == 'WEEK') list = weekList;
+	else if (scope == 'MONTH') list = monthList;
+	
+	targetDate = taggetting(list, direction); 		// 타겟 구하기 
+	db_selectSearch(scope, targetDate); 			// 결과 출력 호출
+};
+
+// 좌/우 nav 타겟 구하는 함수
+var taggetting = function (list, direction) {
+	var lastTagget;
+	(scope == 'DAY') ? lastTagget = firstResultDate : lastTagget = targetDate ;
+	targetDate = list[$.inArray(lastTagget, list) + direction];
+	navDisplay(list); 										// 좌우 화살표 출력 판단
+	return targetDate;
+};
+
+// 좌우 화살표 출력 판단
+var navDisplay = function (list) {
 	(targetDate == list[0]) ? $('#date .left').css('display', 'none') : $('#date .left').css('display', 'block');
 	(targetDate == list[list.length-1]) ? $('#date .right').css('display', 'none') : $('#date .right').css('display', 'block');
-}
+};
 
-// 결과를 HTML에 출력
-function db_listing(res, scope) {
+
+// 결과를 Text list로 출력
+var db_listing = function (res, scope) {
 	var len = res.rows.length;
 	console.log("ACTION (page): " + len + " rows found.");
 	
-	resultDate = res.rows.item(0).strtDay;
+	firstResultDate = res.rows.item(0).strtDay;
 	
 	if (scope == 'LASTDAY') {
-		$('#date>p').text(resultDate.replace(/-/g, '/').substring(5)); 	// 날짜 출력
-		($.inArray(resultDate, dayList) > 0) ? $('#date .left').css('display', 'block') : $('#date .left').css('display', 'none');
+		$('#date>p').text(firstResultDate.replace(/-/g, '/').substring(5)); 	// 날짜 출력
+		($.inArray(firstResultDate, dayList) > 0) ? $('#date .left').css('display', 'block') : $('#date .left').css('display', 'none');
 		$('#date .right').css('display', 'none');
 		
 	} else if (scope == 'DAY') {
-		$('#date>p').text(resultDate.replace(/-/g, '/').substring(5)); 	// 날짜 출력
-		
+		$('#date>p').text(targetDate.replace(/-/g, '/').substring(5)); 	// 날짜 출력
 	} else if (scope == 'WEEK') {
-		$('#date>p').text(resultDate.replace(/-/g, '/').substring(5) + ' ~일주일');
-		
+		$('#date>p').text(targetDate.replace(/-/g, '/').substring(5) + ' ~일주일');
 	} else if (scope == 'MONTH')  {
-		$('#date>p').text(resultDate.replace(/-/g, '/').substring(0, 7));
-		
+		$('#date>p').text(targetDate.replace(/-/g, '/').substring(0, 7));
 	}
 	
 	var resultList = $('#resultList');
@@ -133,23 +135,24 @@ function db_listing(res, scope) {
 			whileT = Math.floor(whileT/3600) + '시간 ' + Math.floor(whileT%3600/60) + '분 ' + whileT%60 + '초';
 		}
 		
-		startTime = res.rows.item(i).START_TIME;
-		endTime = res.rows.item(i).END_TIME;
+		var startTime = res.rows.item(i).START_TIME;
+		var endTime = res.rows.item(i).END_TIME;
 		
-		if (endTime == null) break;
+		if (endTime == null) break; 			// 진행중인것 출력 방지
 		
 		resultList.append($('<div class="rtTable">')
-				.append('<div data-id= "'+res.rows.item(i).ID +'" class="rtIcon actionName">'+'<i class= "'+res.rows.item(i).CLASSNAME+'"></i></div>')
+				.append('<div data-id= "'+res.rows.item(i).ID +'" class="rtIcon">'+'<i class= "'+res.rows.item(i).CLASSNAME+'"></i></div>')
 				.append('<div class="rtTime">' + startTime.substring(11, 16) + ' ~ ' + endTime.substring(11, 16))
 				.append('<div class="rtDuration">' + whileT +'</div>')
 				.append('<div class="rtDelete"><i class="fa fa-times"></i></div>')
 		);
 	}
-}
+};
 
-function db_selectSearch(scope, date) { // 날짜, 범위 받고 쿼리 수행
+// 날짜, 범위 받고 쿼리 수행
+var db_selectSearch = function (scope, target) { 
 	if (scope == 'LASTDAY') { 	// 마지막 날짜 출력
-		lastActionSql = "(SELECT date(START_TIME) AS stDay FROM ACTION ORDER BY START_TIME DESC LIMIT 1)";
+		var lastActionSql = "(SELECT date(START_TIME) AS stDay FROM ACTION ORDER BY START_TIME DESC LIMIT 1)";
 		db.transaction(function(tx) {
 			tx.executeSql("SELECT *, strftime('%Y-%m-%d', START_TIME) AS strtDay FROM ACTION "
 				+" WHERE START_TIME BETWEEN date("+lastActionSql+") AND date("+lastActionSql+", ?) ORDER BY START_TIME", ['+1 day'], function(tx, res) {
@@ -158,6 +161,7 @@ function db_selectSearch(scope, date) { // 날짜, 범위 받고 쿼리 수행
 		}, db_errorCB);
 		
 	} else { 	// 범위에 따른 WHERE 문 선택
+		var whereSql = '';
 		if (scope == 'DAY') {
 			whereSql = " WHERE START_TIME BETWEEN date(?) AND date(?, '+1 day') ORDER BY START_TIME";
 		} else if (scope == 'MONTH') {
@@ -170,24 +174,23 @@ function db_selectSearch(scope, date) { // 날짜, 범위 받고 쿼리 수행
 		}
 		
 		db.transaction(function(tx) {
-			tx.executeSql("SELECT *, strftime('%Y-%m-%d', START_TIME) AS strtDay FROM ACTION " + whereSql, [date, date], function(tx, res) {
+			tx.executeSql("SELECT *, strftime('%Y-%m-%d', START_TIME) AS strtDay FROM ACTION " + whereSql, [target, target], function(tx, res) {
 				db_listing(res, scope);
 			});
 		}, db_errorCB);
 	}
 };
 
-
-
-// 페이징 용 날짜 목록 만들기
-function db_dayList() { 
+// nav가 사용할 날짜 목록 만들기
+var db_dayList = function () { 
 	db.transaction(function(tx) {
 		tx.executeSql("SELECT strftime('%Y-%m-%d', START_TIME) AS strtDay, strftime('%Y-%m', START_TIME) AS strtMonth FROM ACTION ORDER BY strtDay", [], function(tx, res) {
 			var len = res.rows.length;
 			console.log("ACTION (All): " + len + " rows found.");
 			
 			for (var i=0; i<len; i++){
-				if ($.inArray(res.rows.item(i).strtDay, dayList) == -1) {	// 일 목록(dayList) 만들기
+				// 일 목록(dayList) 만들기
+				if ($.inArray(res.rows.item(i).strtDay, dayList) == -1) {	
 					dayList.push(res.rows.item(i).strtDay);
 					
 					// 주 목록(weekList) 만들기
@@ -206,39 +209,38 @@ function db_dayList() {
 };
 
 // 결과 하나 지우기
-function db_delete(no){
+var db_delete = function (no){
 	db.transaction(function(tx){
 		tx.executeSql("DELETE FROM ACTION WHERE ID = ?",[no]);
 	});
-}
-
+};
 
 //query 에러시 호출 함수
-function db_errorCB(e) {
+var db_errorCB = function (e) {
 	console.log(e);
 	console.log("e.message :" + e.message);
-}
-
+};
 
 // 00:00:00 형태 만들기 함수
 var pad = function (n){return n<10 ? '0'+n : n;};
 
 // 월~일 범위 중 월요일 얻기
-function getLastMonday(d) {
+var getLastMonday = function (d) {
 	var t = new Date(d);
 	var weekDay  = t.getDay();
 	t.setDate(t.getDate() - t.getDay() + (weekDay === 0 ? -6 : 1));
 	return t;
 	//return new Date(t.getTime() + t.getTimezoneOffset()*60000);
-}
+};
+
 // 일요일 얻기
-function getNextSunday(d) {
+var getNextSunday = function (d) {
 	var t = new Date(d);
 	var weekDay  = t.getDay();
 	t.setDate(t.getDate() - t.getDay() + (weekDay === 0 ? 0 : 7));
 	return t;
 	//return new Date(t.getTime() + t.getTimezoneOffset()*60000);
-}
+};
 
 // local 날짜 형태 만들기 (2014-06-19T14:12:35.261)
 var localISOString = function(d) {
--- DragLogSVN/WebContent/www/js/resultPie.js
+++ DragLogSVN/WebContent/www/js/resultPie.js
@@ -8,76 +8,12 @@ $(window).load(function(){
 	
 	//가입되어있으면 통계 제공
 	db_resultPie();
-	//$("#doughnutChart").drawDoughnutChart(result);
 });
 
-var data = [
-    	    { title: "Tokyo",         value : 120,  color: "#2C3E50" },
-    	    { title: "San Francisco", value:  80,   color: "#FC4349" },
-    	    { title: "New York",      value:  70,   color: "#6DBCDB" },
-    	    { title: "London",        value : 50,   color: "#F7E248" },
-    	    { title: "Sydney",        value : 40,   color: "#D7DADB" },
-    	    { title: "Berlin",        value : 20,   color: "#FFF" }
-];
-
-
-console.log(data);
-
-//============================================================
-
-// 데이터 가공
+// 임시 전체 출력
 function db_resultPie() {
 	db.transaction(function(tx) {
-		tx.executeSql("select * from ACTION ", [], function(tx, res) {
-			var len = res.rows.length;
-			console.log("ACTION: " + len + " rows found.");
-			
-			var resultObj = new Object();
-			for (var i=0; i<len; i++) { 
-				if (res.rows.item(i).END_TIME == null) {
-					console.log(res.rows.item(i).END_TIME);
-					break;
-				}
-				if (resultObj[res.rows.item(i).TITLE] == undefined) {
-					resultObj[res.rows.item(i).TITLE] = res.rows.item(i).WHILE; // 같은 값 없으면 저장
-				} else {
-					resultObj[res.rows.item(i).TITLE] += res.rows.item(i).WHILE; // 같은 값 있으면 합산
-				}
-			}
-			console.log(resultObj);
-			
-			
-			// 결과를 값 큰 순서로 정렬
-			var sortResult =[];
-			for (var title in resultObj) {
-				sortResult.push([title, resultObj[title]]);
- 			};
- 			sortResult.sort(function (a, b) {return b[1] - a[1];});
-			console.log(sortResult);
-			
-			
-			// 결과를 출력 함수가 원하는 배열[obj, obj ... ] 형태로 생성 
-			var tempC = '';
-			for (var i=0; i<sortResult.length ; i++) {
-				var tempObj = new Object();
-				tempObj['title'] = sortResult[i][0];
-				tempObj['value'] = sortResult[i][1];
-				
-				if (i < 10 ) {
-					tempObj['color'] =  "#".concat(i,i,i);
-				} else {
-					tempC = i.toString().substring(1);
-					tempObj['color'] =  "#".concat(tempC,tempC,tempC);
-				}
-				//console.log(tempObj);
-				result.push(tempObj);
-			}
-			console.log(result);
-			
-			
-			// 실제 차트 그리기
-			$("#doughnutChart").drawDoughnutChart(result);
-		});
+		tx.executeSql("select * from ACTION ", [], resultProcess);
 	}, db_errorCB);
 }
 
@@ -86,11 +22,54 @@ function db_errorCB(e) { // query 에러시 호출 함수
 	console.log("e.message :" + e.message);
 }
 
-// Object size 구하기
-getObjLength = function(obj) {
-    var size = 0, key = null;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
+//============================================================
+// 결과 가공 후 출력
+var resultProcess = function(tx, res) {
+	var len = res.rows.length;
+	console.log("ACTION: " + len + " rows found.");
+	
+	var resultObj = new Object();
+	for (var i=0; i<len; i++) { 
+		if (res.rows.item(i).END_TIME == null) {
+			console.log(res.rows.item(i).END_TIME);
+			break;
+		}
+		if (resultObj[res.rows.item(i).TITLE] == undefined) {
+			resultObj[res.rows.item(i).TITLE] = res.rows.item(i).WHILE; // 같은 값 없으면 저장
+		} else {
+			resultObj[res.rows.item(i).TITLE] += res.rows.item(i).WHILE; // 같은 값 있으면 합산
+		}
+	}
+	console.log(resultObj);
+	
+	// 결과를 값 큰 순서로 정렬
+	var sortResult =[];
+	for (var title in resultObj) {
+		sortResult.push([title, resultObj[title]]);
+		};
+		sortResult.sort(function (a, b) {return b[1] - a[1];});
+	console.log(sortResult);
+	
+	
+	// 결과를 출력 함수가 원하는 배열[obj, obj ... ] 형태로 생성 
+	var tempC = '';
+	for (var i=0; i<sortResult.length ; i++) {
+		var tempObj = new Object();
+		tempObj['title'] = sortResult[i][0];
+		tempObj['value'] = sortResult[i][1];
+		
+		if (i < 10 ) {
+			tempObj['color'] =  "#".concat(i,i,i);
+		} else {
+			tempC = i.toString().substring(1);
+			tempObj['color'] =  "#".concat(tempC,tempC,tempC);
+		}
+		//console.log(tempObj);
+		result.push(tempObj);
+	}
+	console.log(result);
+	
+	
+	// 실제 차트 그리기
+	$("#doughnutChart").drawDoughnutChart(result);
 };
