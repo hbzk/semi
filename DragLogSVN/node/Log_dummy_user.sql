3dde08aee72d6c4e4562365930347819173d8e3b
SUNGHWANO <SUNGHWANO@users.noreply.github.com>
2014-07-08 18:03:46 +0900

    ..

---
+++ DragLogSVN/node/Log_dummy_user.sql
@@ -0,0 +1,31 @@
+-- USER DUMMY ============================================================
+
+INSERT IGNORE INTO USER (EMAIL, GENDER, AGE, JOB, SALARY, SPEND, SCHOLAR, MARRY)
+
+VALUES 
+
+ 
+
+,("kokore@gd.com", 2, 31, 2, 5, 250, 4, 1)
+
+,("uverworld@yahoo.com", 2, 32, 2, 5, 260, 4, 2)
+
+,("SuranIbrahim@cb.com", 2, 33, 2, 5, 220, 4, 2)
+
+,("option@naver.com", 2, 33, 2, 5, 240, 4, 1)
+
+,("kim04@gmail.com", 2, 31, 2, 5, 220, 4, 1)
+
+,("ultra7@paran.com", 1, 32, 2, 5, 210, 4, 1)
+
+,("r2d2@ezweb.ne", 1, 33, 2, 5, 220, 4, 1)
+
+,("alucard@daum.net", 1, 32, 2, 5, 235, 4, 2)
+
+,("eastsea@nate.com", 1, 31, 2, 5, 215, 4, 1)
+
+,("killthe11@daum.net", 1, 31, 2, 5, 255, 4, 2)
+
+,("usoda@naver.com", 1, 31, 2, 5, 210, 4, 2)
+
+ 
